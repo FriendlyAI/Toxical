bfb07d755d33588703b06415138acea804a8c877
Andrew <li_andrewy@protonmail.com>
2018-03-24 15:15:42 -0400

    read csv file method

--- train.py
+++ train.py
@@ -1,11 +1,12 @@
 import numpy as np
 import tensorflow as tf
 import re
-
+import csv
 
 WORDS_LIST = np.load('wordsList.npy').tolist()
 WORDS_LIST = [word.decode('UTF-8') for word in WORDS_LIST]  # Encode words as UTF-8
 WORD_VECTORS = np.load('wordVectors.npy')
+
 
 def main():
     # wordsList = np.load('wordsList.npy')
@@ -16,9 +17,12 @@ def main():
     # print('Loaded the word vectors!')
 
     # baseballIndex = wordsList.index('india')
-    # print(wordVectors[baseballIndex])
-    while True:
-        vectorize(clean(input()))
+    # print(wordVectors[baseballIndex])])
+
+    # while True:
+    #     vectorize(clean(input()))
+
+    read_csv()
 
 
 def vectorize(sentence_array):
@@ -26,6 +30,13 @@ def vectorize(sentence_array):
         index = WORDS_LIST.index(word)
         vector = WORD_VECTORS[index]
         print(vector)
+
+
+def read_csv():
+    with open('twitter_data.csv', newline='') as file:
+        reader = csv.reader(file)
+        for line in reader:
+            print(line[0], clean(line[1]))
 
 
 def clean(sentence):
