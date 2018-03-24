6c235c8418c3bcf2da8c4ad9c1575020b1aa5357
Andrew <li_andrewy@protonmail.com>
2018-03-24 14:28:12 -0400

    Clean sentence method

--- train.py
+++ train.py
@@ -1,17 +1,26 @@
 import numpy as np
 import tensorflow as tf
+import re
 
 
 def main():
-    wordsList = np.load('wordsList.npy')
-    print('Loaded the word list!')
-    wordsList = wordsList.tolist()  # Originally loaded as numpy array
-    wordsList = [word.decode('UTF-8') for word in wordsList]  # Encode words as UTF-8
-    wordVectors = np.load('wordVectors.npy')
-    print('Loaded the word vectors!')
+    # wordsList = np.load('wordsList.npy')
+    # print('Loaded the word list!')
+    # wordsList = wordsList.tolist()  # Originally loaded as numpy array
+    # wordsList = [word.decode('UTF-8') for word in wordsList]  # Encode words as UTF-8
+    # wordVectors = np.load('wordVectors.npy')
+    # print('Loaded the word vectors!')
+    #
+    # baseballIndex = wordsList.index('india')
+    # print(wordVectors[baseballIndex])
+    while True:
+        print(clean(input()))
 
-    baseballIndex = wordsList.index('india')
-    print(wordVectors[baseballIndex])
+
+def clean(sentence):
+    special_chars = re.compile(r'[^a-zA-Z0-9]+')
+    cleaned = re.sub(special_chars, ' ', sentence)
+    return [cleaned_word.lower() for cleaned_word in cleaned.split()]
 
 
 if __name__ == '__main__':
