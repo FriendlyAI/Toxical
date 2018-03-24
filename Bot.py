3ac25868a090ea631f0ff81b1c2af0d300d80d33
Unknown <justineli884@gmail.com>
2018-03-24 15:18:16 -0400

    Echo

--- Bot.py
+++ Bot.py
@@ -9,26 +9,26 @@ TOKEN = "NDI3MTQ3Mjc0NDk4MzQyOTMy.DZgT3g.UwYjlweXBF0b1X03r74lUt-v1ms"  # Get at 
 
 client = Bot(command_prefix=BOT_PREFIX)
 
-@client.command(name='8ball',
-                description="Answers a yes/no question.",
-                brief="Answers from the beyond.",
-                aliases=['eight_ball', 'eightball', '8-ball'],
-                pass_context=True)
-async def eight_ball(context):
-    possible_responses = [
-        'That is a resounding no',
-        'It is not looking likely',
-        'Too hard to tell',
-        'It is quite possible',
-        'Definitely',
-    ]
-    await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
-
-
-@client.command()
-async def square(number):
-    squared_value = int(number) * int(number)
-    await client.say(str(number) + " squared is " + str(squared_value))
+# @client.command(name='8ball',
+#                 description="Answers a yes/no question.",
+#                 brief="Answers from the beyond.",
+#                 aliases=['eight_ball', 'eightball', '8-ball'],
+#                 pass_context=True)
+# async def eight_ball(context):
+#     possible_responses = [
+#         'That is a resounding no',
+#         'It is not looking likely',
+#         'Too hard to tell',
+#         'It is quite possible',
+#         'Definitely',
+#     ]
+#     await client.say(random.choice(possible_responses) + ", " + context.message.author.mention)
+#
+#
+# @client.command()
+# async def square(number):
+#     squared_value = int(number) * int(number)
+#     await client.say(str(number) + " squared is " + str(squared_value))
 
 
 @client.event
@@ -37,12 +37,12 @@ async def on_ready():
     print("Logged in as " + client.user.name)
 
 
-@client.command()
-async def bitcoin():
-    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
-    response = requests.get(url)
-    value = response.json()['bpi']['USD']['rate']
-    await client.say("Bitcoin price is: $" + value)
+# @client.command()
+# async def bitcoin():
+#     url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+#     response = requests.get(url)
+#     value = response.json()['bpi']['USD']['rate']
+#     await client.say("Bitcoin price is: $" + value)
 
 
 async def list_servers():
@@ -55,7 +55,8 @@ async def list_servers():
 
 @client.event
 async def on_message(message):
-    await client.send_message(message.channel, message)
+    if message.author.id != client.user.id:
+        await client.send_message(message.channel, message.content)
 
 client.loop.create_task(list_servers())
 client.run(TOKEN)
