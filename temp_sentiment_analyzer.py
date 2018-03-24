1f5652ccfc91df2783a592482e53bf656bc09f41
Andrew <li_andrewy@protonmail.com>
2018-03-24 19:34:58 -0400

    Temp sentiment analyzer

---
+++ temp_sentiment_analyzer.py
@@ -0,0 +1,21 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import re
+
+analyzer = SentimentIntensityAnalyzer()
+
+# while True:
+#     sentence = input()
+#     vs = analyzer.polarity_scores(sentence)
+#     print("{:-<65} {}".format(sentence, str(vs)))
+
+
+def analyze(sentence):
+    sentence = clean(sentence)
+    vs = analyzer.polarity_scores(sentence)
+    print(sentence, vs)
+
+
+def clean(sentence):
+    special_chars = re.compile(r'[^a-zA-Z]+')
+    cleaned = re.sub(special_chars, ' ', sentence)
+    return ' '.join([cleaned_word.lower() for cleaned_word in cleaned.split()])
