b3aaa9fce31b5dcdcb9cc8b03d3e97822c1588e7
Andrew <li_andrewy@protonmail.com>
2018-03-25 03:39:19 -0400

    Auto stash before merge of "Andrew" and "master"

--- Bot.py
+++ Bot.py
@@ -5,14 +5,16 @@ from discord import Game
 from discord.ext.commands import Bot
 from pymongo import MongoClient
 from temp_sentiment_analyzer import analyze
+from collections import deque
 
 BOT_PREFIX = ("?", "!")
-TOKEN = "NDI3MTQ3Mjc0NDk4MzQyOTMy.DZgT3g.UwYjlweXBF0b1X03r74lUt-v1ms"  # Get at discordapp.com/developers/applications/me
+TOKEN = "NDI3MTQ3Mjc0NDk4MzQyOTMy.DZgT3g.UwYjlweXBF0b1X03r74lUt-v1ms" # Get at discordapp.com/developers/applications/me
 
 client = Bot(command_prefix=BOT_PREFIX)
 c = MongoClient()
 db = c['toxicity']
 
+spoints = {}
 
 @client.event
 async def on_ready():
@@ -25,6 +27,7 @@ async def on_ready():
             ser.insert_one({"Servers":
                                 {"SID": servers[x].id, "users":
                                     {"UID": member.id, "points":  100}}})
+
 
 # @client.command()
 # async def bitcoin():
@@ -45,8 +48,14 @@ async def list_servers():
 @client.event
 async def on_message(message):
     if message.author.id != client.user.id:
-        message_toxicity = analyze(message.content)
-        await client.send_message(message.channel, message_toxicity)
+        message_toxicity_string, toxicity_dict = analyze(message.content)
+        await client.send_message(message.channel, message_toxicity_string)
+        if message.author.server.id not in spoints:
+            spoints[message.author.server.id] = []
+        if len(spoints[message.author.server.id]) > 100:
+            spoints.get(message.author.server.id).pop()
+        spoints.get(message.author.server.id).append(message.content)
+
 
 client.loop.create_task(list_servers())
 client.run(TOKEN)
